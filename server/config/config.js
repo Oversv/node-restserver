76693de29b5e889146df88718541a605f50ce919
Oversv <albertomdv@gmail.com>
2020-02-10 18:08:43 +0000

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+//============
+//PUERTO
+//============
+
+//Lee el puerto donse se está ejecutando, en caso se que no tenga ningún puerto se le asigna el 3000  
+process.env.PORT = process.env.PORT || 3000
